66ef5e8ebee5f949ac503c8878b9664ebc6e87c3
Christopher Schwab <cschwab24@gmail.com>
2017-09-06 22:34:44 -0400

    nav layout, slick slider working for hero

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
-	
+	// hero carousel
+	$("#hero .slider").slick({
+		dots: false,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000
+	});
 	
 }); // end doc ready
