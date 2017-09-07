735c7e6f8962c0baa02f5139cc84413a33fb71a5
Christopher Schwab <cschwab24@gmail.com>
2017-09-07 00:38:18 -0400

    recommendations carousel

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	// hero carousel
+	// hero slider
 	$("#hero .slider").slick({
 		dots: false,
 		infinite: true,
@@ -9,6 +9,45 @@ $(document).ready(function(){
 		slidesToScroll: 1,
 		autoplay: true,
 		autoplaySpeed: 3000
+	});
+
+	// recommendations carousel
+	$('#recommendations .carousel').slick({
+	  dots: false,
+	  infinite: false,
+	  speed: 300,
+	  slidesToShow: 7,
+	  slidesToScroll: 7,
+	  responsive: [
+	  	{
+	  	  breakpoint: 1280,
+	  	  settings: {
+	  	    slidesToShow: 5,
+	  	    slidesToScroll: 5,
+	  	  }
+	  	},
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 4,
+	        slidesToScroll: 4,
+	      }
+	    },
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 3
+	      }
+	    },
+	    {
+	      breakpoint: 540,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	  ]
 	});
 	
 }); // end doc ready
